6272752d13f4f5827f01689966dff8abd5fe2f8a
Sotaneum <gnyontu39@gmail.com>
2022-09-30 11:21:48 +0900

    환경 변수에서 port를 가져오도록 수정

---
+++ src/demoList.ts
@@ -0,0 +1,10 @@
+const DEMO_PATH = {
+  "/": "index.html",
+  "/socket.io.js": "../node_modules/socket.io/client-dist/socket.io.js",
+  "/RTCMultiConnection.js": "../public/RTCMultiConnection.js",
+  "/socket.io.js.map": "../node_modules/socket.io/client-dist/socket.io.js.map",
+  "/adapter.js": "../node_modules/webrtc-adapter/out/adapter.js",
+  "/FileBufferReader.js": "../node_modules/fbr/FileBufferReader.js",
+};
+
+export default DEMO_PATH;
